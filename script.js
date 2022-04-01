af5a42c8d1cf26371f7b7ff42c2892b08d8b0eed
shan1y <shannonyazdani@gmail.com>
2022-03-31 21:26:04 -0400

    Finalized

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 let btn = document.querySelector('button')
+let error_text = document.querySelectorAll('.error_text')
+let error_icon = document.querySelectorAll('.icon')
+var mailformat =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-btn.addEventListener("click",(e) => {
-    console.log('hi')
-})
+
+const inputs = document.getElementsByTagName('input');
+
+function validateInputs() {
+    btn.addEventListener('click', function () {
+            console.log('hi')
+            
+       for (let i=0; i< inputs.length; i++) {     
+           
+        if(i===2) {
+            if(inputs[2].value.match(mailformat)) {
+                error_text[i].classList.add('hidden');
+                error_icon[i].classList.add('hidden');
+                } else {
+                error_text[i].classList.remove('hidden');
+                error_icon[i].classList.remove('hidden');
+                }
+            }
+
+            else {
+                if (!inputs[i].value) {
+                error_text[i].classList.remove('hidden');
+                error_icon[i].classList.remove('hidden');
+            } else {
+                error_text[i].classList.add('hidden');
+                error_icon[i].classList.add('hidden');
+                
+            }
+        }
+    }
+    })
+}
+
+validateInputs()
